5d2846fc3f492991ad0b8ef758909b6a1e038b99
TTPO100AJIEX <ttpo100ajiex@mail.ru>
2025-03-09 02:09:55 +0300

    Part 5: remove some functions

--- utils/extract_call_contents.py
+++ utils/extract_call_contents.py
@@ -9,13 +9,14 @@ def extract_call_contents(event_log_list: typing.List[dict], call_name_substring
     for event in tqdm.tqdm(event_log_list):
         event_name = event['concept:name']
 
-        if call_name_substring in event_name:
-            if event_name.startswith("Start:"):
+        if event_name.endswith(call_name_substring):
+            if event_name == f"Start:{call_name_substring}":
                 open_starts += 1
             else:
-                assert event_name.startswith("End:")
+                assert event_name == f"End:{call_name_substring}"
                 open_starts -= 1
                 if open_starts == 0:
+                    log_list_filt.append({ **event, 'case:concept:name': str(call_number) })
                     call_number += 1
 
         if open_starts != 0:
